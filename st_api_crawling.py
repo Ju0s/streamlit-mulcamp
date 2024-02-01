e8aa6dcfa21ff9de0d84d626581fbb12ee2437f0
juyoung <juyoungeeya@gmail.com>
2024-02-01 13:45:18 +0900

    UPDATED: streamlit secrets

--- st_api_crawling.py
+++ st_api_crawling.py
@@ -1,12 +1,9 @@
 import requests
 import json
 import pandas as pd
-from dotenv import load_dotenv
-import os
 import streamlit as st
 
-load_dotenv()
-SERVICE_KEY = os.getenv('SERVICE_KEY')
+SERVICE_KEY = st.secrets['SERVICE_KEY']
 
 def fetch_data(start, end):
     URL = f'http://openapi.seoul.go.kr:8088/{SERVICE_KEY}/json/tbLnOpendataRtmsV/{start}/{end}/'
